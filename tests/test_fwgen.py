c1c4c9e525c81d44cc2a5dfc49062d6f5b166f8f
hk <hkbakke@gmail.com>
2017-08-03 18:32:17 +0200

    Add tests for _get_policy_rules

--- tests/test_fwgen.py
+++ tests/test_fwgen.py
@@ -66,3 +66,69 @@ class TestFwGen(object):
 
         result = fw._substitute_variables(rule)
         assert result == rule_substituted
+
+    def test_get_policy_rules(self):
+        config = {
+            'global': {
+                'policy': {
+                    'filter': {
+                        'INPUT': 'DROP',
+                        'OUTPUT': 'DROP'
+                    },
+                }
+            }
+        }
+        fw = fwgen.FwGen(config)
+        policy_rules = [
+            ('filter', ':INPUT DROP'),
+            ('filter', ':FORWARD ACCEPT'),
+            ('filter', ':OUTPUT DROP'),
+            ('nat', ':PREROUTING ACCEPT'),
+            ('nat', ':INPUT ACCEPT'),
+            ('nat', ':OUTPUT ACCEPT'),
+            ('nat', ':POSTROUTING ACCEPT'),
+            ('security', ':INPUT ACCEPT'),
+            ('security', ':FORWARD ACCEPT'),
+            ('security', ':OUTPUT ACCEPT'),
+            ('mangle', ':PREROUTING ACCEPT'),
+            ('mangle', ':INPUT ACCEPT'),
+            ('mangle', ':FORWARD ACCEPT'),
+            ('mangle', ':OUTPUT ACCEPT'),
+            ('mangle', ':POSTROUTING ACCEPT'),
+            ('raw', ':PREROUTING ACCEPT'),
+            ('raw', ':OUTPUT ACCEPT'),
+        ]
+        assert sorted([i for i in fw._get_policy_rules()]) == sorted(policy_rules)
+
+    def test_get_policy_rules_reset(self):
+        config = {
+            'global': {
+                'policy': {
+                    'filter': {
+                        'INPUT': 'DROP',
+                        'OUTPUT': 'DROP'
+                    },
+                }
+            }
+        }
+        fw = fwgen.FwGen(config)
+        policy_rules = [
+            ('filter', ':INPUT ACCEPT'),
+            ('filter', ':FORWARD ACCEPT'),
+            ('filter', ':OUTPUT ACCEPT'),
+            ('nat', ':PREROUTING ACCEPT'),
+            ('nat', ':INPUT ACCEPT'),
+            ('nat', ':OUTPUT ACCEPT'),
+            ('nat', ':POSTROUTING ACCEPT'),
+            ('security', ':INPUT ACCEPT'),
+            ('security', ':FORWARD ACCEPT'),
+            ('security', ':OUTPUT ACCEPT'),
+            ('mangle', ':PREROUTING ACCEPT'),
+            ('mangle', ':INPUT ACCEPT'),
+            ('mangle', ':FORWARD ACCEPT'),
+            ('mangle', ':OUTPUT ACCEPT'),
+            ('mangle', ':POSTROUTING ACCEPT'),
+            ('raw', ':PREROUTING ACCEPT'),
+            ('raw', ':OUTPUT ACCEPT'),
+        ]
+        assert sorted([i for i in fw._get_policy_rules(reset=True)]) == sorted(policy_rules)
